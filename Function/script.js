5cedb678e8586c3eebb9ebbe0cb17e350b396d18
Almiradnan <almiraadnan34@gmail.com>
2024-07-27 20:09:24 +0500

    done

---
+++ Function/script.js
@@ -0,0 +1,74 @@
+console.log("Function Chapter 35")
+
+// var now = new Date();
+// var theHr = now.getHours();
+// var theMin = now.getMinutes();
+// alert("time: " + theHr + ":" + theMin);
+
+// Function Syntax
+
+// function getTime() {
+//     console.log("Kashan")
+// }
+
+// // Function Call
+// // to call / run the function
+// getTime() // 1
+// getTime() // 2
+
+
+// greetUser
+// var Global Varible Javascript Available
+
+// Block Scope
+// const contstant
+// Blocked Scope Varible
+// var global Varible
+
+// const a = "Kashan Adnan" // Block ScopeVarible
+
+// console.log(a);
+// let a = "Aniq"
+// console.log(a);
+// function greetUser() {
+//     let user_name = prompt("Enter your name : ")
+//     alert("Hello " + user_name)
+// }
+// console.log(user_name);
+// greetUser()
+
+
+// const , let , var
+
+// function greetUser(name , age , dob) { // Get Parameter
+//    console.log(name)
+//    console.log(age)
+//    console.log(dob)
+// }  
+
+// greetUser("Kashan", 12 , 2011) // Pass Paramters
+
+// function throwError(){
+
+//     return 
+// }
+
+// console.log(throwError())
+
+
+// Get user name
+
+// let user_name = prompt("Enter your name : ")
+
+// function greetUser(name) {
+//     return "Wasay"
+// }
+
+// let name = greetUser(user_name)
+
+// console.log(name)
+// let name = "Kashan" // global
+// function getName() {
+//     let name = "Kashan" // local
+// }
+
---
+++ assigment.js
@@ -0,0 +1,7 @@
+// Write a program that uses the date to print the current date
+
+// Create a program jo ek promt me date ese lega "MM/DD/YYYY" phir use is format me convert karna he "YYYY-MM-DD".
+
+// Write a program that takes a birth year lena he prompt or calculate karna he age ko 
+
+// Hint : https://github.com/KashanAdnan-Coder/Javascript/tree/main/Date%20Chapter%2031
