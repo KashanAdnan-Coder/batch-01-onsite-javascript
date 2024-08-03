60e310a91e2bacfec1c833c7ea15435916b09069
KashanAdnan-Coder <computertechub0@gmail.com>
2024-08-03 20:06:06 +0500

    mark generator

--- Events/script.js
+++ Events/script.js
@@ -2,4 +2,20 @@
 //     alert("Hi, How Are you!")
 // }
 
-console.log(document.getElementById("name").value);
+
+
+function getInputValue() {
+    let inputValue = document.getElementById("name")
+    console.log(inputValue.value);
+}
+
+function generateMarks() {
+    let english = document.getElementById("eng_marks").value;
+    let urdu = document.getElementById("urdu_marks").value;
+    let science = document.getElementById("sci_marks").value;
+    let total = parseInt(english) + parseInt(urdu) + parseInt(science)
+
+    let percentage = (total / 500) * 100 // 100 
+    console.log(Math.round(percentage) + "%");
+
+}
