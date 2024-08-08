4e23bdf3cc8105ff83eb19b90a3311671a87b7af
KashanAdnan-Coder <computertechub0@gmail.com>
2024-08-08 21:29:09 +0500

    assigment of 10 -8 24 ka kaam he check hoga

---
+++ Revision/script.js
@@ -0,0 +1,52 @@
+// for loop syntax  (method) (likhne ka tarika)
+// 3 statement
+// declaration let a = 0
+// condition  a < 9
+// iteration a++ = a + 1
+// for (let a = 0; a < 8; a++) {
+//     // Code Execute
+//     console.log("Kashan");
+//     //     // Condition False
+// }
+
+// short and easy
+
+// 3 statement
+// declaration let a = 0
+// condition  a < 9
+// iteration a++ = a + 1
+// for loop
+// while loop 
+// while (keyword)
+// for (keyword)
+// let i = 0; //
+// while (i < 2) { // condition true
+//     console.log(i) // 1
+//     i++ // 1 + 1 =  2
+// }
+// break
+
+// do while (karna) (keyword)
+
+// declaration let a = 0
+// condition  a < 9
+// iteration a++ = a + 1
+// let i = 0
+// do {
+//     console.log("Kashan")
+//     i++
+// } while (i < 2)
+let array = ["Kashan", "Fatima", "Adnan"]
+let i = 0
+while (i < 2) {
+    if (array[i] == "Fatima") {
+      
+    }
+    i++
+}
+
+// write a line 
+// jo array ke element ka index bataye 
+// abhi humlogon ne fatima ko dhonda 
+// to fatima ka index ana chahiye
+// agar hum Kashan check karen to kashan ka  index aye
