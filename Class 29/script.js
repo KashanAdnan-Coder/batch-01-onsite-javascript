4f51065b96f57c0d0cc5095a3278c9ba6f33092d
KashanAdnan-Coder <computertechub0@gmail.com>
2024-09-14 21:34:37 +0500

    some-code

---
+++ Class 29/script.js
@@ -0,0 +1,43 @@
+// let array = [90, 80, 20, 40, 50]
+
+// let arrayFind = array.find(element => element < 50)
+
+// console.log(arrayFind);
+
+
+// findIndex Element ke index ko find karta he
+
+// let array = ["Kashan", "Wasay", "Aniq"]
+
+// let findIndex = array.findIndex(element => element === "Kashan")
+
+// console.log(findIndex);
+
+// index Of
+
+// array ke andar ke vo element 
+
+// false -1
+// true 1
+
+// let arr = ["Kashan", "John", "Aniq"]
+
+// let findIndexOF = arr.indexOf("Wasay") // -1
+// let findIndexOF1 = arr.indexOf("John") // 1
+// console.log(findIndexOF);
+// console.log(findIndexOF1);
+
+// let arr = ["Kashan", "Kashan"]
+
+// let newArr = arr.fill(3)
+
+// console.log(newArr);
+
+// reverse
+
+// let array = [90, 80, 20, 40, 50]
+
+// array.reverse()
+
+
+// console.log(array);
